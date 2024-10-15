87d2279960bd8d5ddfbdd2627e700368cf2556a0
Rcoder-singh <rishabh22.singh@gmail.com>
2024-10-15 22:09:56 +0530

    Fire flies

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
+import FireFliesBackground from "@/components/FireFliesBackground";
 
 
 const inter = Inter({
@@ -29,6 +30,7 @@ export default function RootLayout({ children }) {
         )}
       >
         {children}
+        <FireFliesBackground/>
       </body>
     </html>
   );
