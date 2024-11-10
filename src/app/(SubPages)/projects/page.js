4bfc079f0247509ae048b00bdc8a608ef73df6b6
Rcoder-singh <rishabh22.singh@gmail.com>
2024-11-10 20:54:53 +0530

    Responsive

--- src/app/(SubPages)/projects/page.js
+++ src/app/(SubPages)/projects/page.js
@@ -11,11 +11,10 @@ export default function Project() {
       <Image
         src={bg}
         alt="background-image"
-        
         className="-z-50 fixed top-0 left-0 w-full h-full object-cover object-center opacity-25"
       />
       <ProjectsList projects={projectsData} />
-      <div className="flex items-center justify-center fixed top-20 -left-24 h-screen">
+      <div className="flex items-center justify-center fixed top-16 lg:top-20 -translate-x-1/2 lg:translate-x-0 -z-10 left-1/2 lg:-left-24 h-screen">
         <RenderModel>
           <Staff />
         </RenderModel>
