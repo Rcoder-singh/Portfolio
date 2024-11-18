0e304118e5460bd536ae597fc8581ad54e66c200
Rcoder-singh <rishabh22.singh@gmail.com>
2024-11-18 23:55:35 +0530

    Project Page Animation

--- src/components/projects/ProjectLayout.jsx
+++ src/components/projects/ProjectLayout.jsx
@@ -1,9 +1,15 @@
+import { motion } from "framer-motion";
 import Link from "next/link";
 import React from "react";
 
+const item = { hidden: { opacity: 0, y: 100 }, show: { opacity: 1, y: 0 } };
+
+const NavLink = motion(Link);
+
 const ProjectLayout = ({ name, description, date, demoLink }) => {
   return (
-    <Link
+    <NavLink
+      variants={item}
       href={demoLink}
       target="_blank"
       className="text-sm md:text-base flex items-center justify-between w-full relative rounded-lg overflow-hidden p-4 md:p-6 custom-bg"
@@ -17,7 +23,7 @@ const ProjectLayout = ({ name, description, date, demoLink }) => {
       <p className="text-muted sm:text-foreground">
         {new Date(date).toDateString()}
       </p>
-    </Link>
+    </NavLink>
   );
 };
 
